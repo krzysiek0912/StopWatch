f2fe0d73dcc139ec963cc8f598bcb694a709d888
Krzysztof Branecki <krzysztof@branecki.net>
2019-08-08 22:48:13 +0200

    Add arrow function to Function Components

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-function Results(props) {
+const Results = props => {
   const results = props.results.map(result => <li key={result}>{result}</li>);
   return <ul className="results">{results}</ul>;
-}
+};
 
 export default Results;
